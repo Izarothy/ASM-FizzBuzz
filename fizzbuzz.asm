4ba3dec2bd2644d9d7f94abc9c9db414c3c58ee0
Izaroth <izarothmail@gmail.com>
2022-01-05 18:56:27 +0100

    First version

--- fizzbuzz.asm
+++ fizzbuzz.asm
@@ -1,54 +1,90 @@
-;Fizzbuzz in ASM
-;Print 1 to N (int)
-;If number divisible by 3 print "fizz"
-;If number divisible by 5 print "buzz"
-;If number divisible by 15 print "fizzbuzz"
-;Otherwise print number
-
-segment .text
+section .text
 	global _start
 
-_start:	
-	;mov edx, fizzlen
-	;mov ecx, fizz
-	;mov ebx, STDOUT
-	;mov eax, SYS_WRITE
-	;0x80
-
-	;mov edx, buzzlen
-	;mov ecx, buzz
-	;mov ebx, STDOUT
-	;mov eax, SYS_WRITE
-	;0x80
-
-	;mov edx, fizzbuzzlen
-	;mov ecx, fizzbuzzlen
-	;mov ebx, STDOUT
-	;mov eax, SYS_WRITE
-	;0x80
+_start:
+	;MOV [num], byte 1
+mainLoop:
+	;Starts at 1
+	ADD [num], byte 1
+	;Change the number below to whatever number of times you want the loop to run
+	CMP [num], byte 100
 	
-	mov edx, len
-	mov ecx, msg
-	mov ebx, STDOUT
-	mov eax, SYS_WRITE
-	0x80
+	JG quit
+	
+	MOV al, [num]
+	XOR ah, ah
+	MOV bl, 15
+	DIV bl
+	CMP ah, 0
+	JE sectFizzBuzz
 
-	mov eax, SYS_EXIT
-	0x80
+	MOV al, [num]
+	XOR ah, ah
+	MOV bl, 3
+	DIV bl
+	CMP ah, 0
+	JE sectFizz
+	
+	MOV al, [num]
+	XOR ah, ah
+	MOV bl, 5
+	DIV bl
+	CMP ah, 0
+	JE sectBuzz
+	
+	JNE sectNeither
+
+	LOOP mainLoop
+sectFizz:
+	MOV edx, fizzLen
+	MOV ecx, fizz
+	MOV ebx, 1
+	MOV eax, 4
+	INT 0x80
+	JMP mainLoop
+sectBuzz:
+	MOV edx, buzzLen
+	MOV ecx, buzz
+	MOV ebx, 1
+	MOV eax, 4
+	INT 0x80
+	JMP mainLoop
+sectFizzBuzz:
+	MOV edx, fizzBuzzLen
+	MOV ecx, fizzBuzz
+	MOV ebx, 1
+	MOV eax, 4
+	INT 0x80
+	JMP mainLoop
+sectNeither:
+	MOV edx, neitherLen
+	MOV ecx, neither
+	MOV ebx, 1
+	MOV eax, 4
+	INT 0x80
+	JMP mainLoop
+quit:
+	MOV edx, len
+	MOV ecx, msg
+	MOV ebx, 1
+	MOV eax, 4
+	INT 0x80
+
+	MOV eax, 1
+	INT 0x80
 section .data
-	SYS_WRITE equ 4
-	SYS_EXIT equ 1
-	STDOUT equ 1
-
-	NUM equ 100
-
-	msg db 'Hello'
+	msg db 'Thanks for coming to my tedtalk',10
 	len equ $ - msg
-	fizz db 'Fizz!'
-	buzz db 'Buzz!'
-	fizzbuzz db 'Fizzbuzz!'
-	
-	fizzlen equ $ - fizz
-	buzzlen equ $ - buzz
-	fizzbuzzlen equ $ - fizzbuzz	
-	
+	num db 0
+
+	fizz db 'Fizz!',10
+	fizzLen equ $ - fizz
+
+	buzz db 'Buzz!',10
+	buzzLen equ $ - buzz
+
+	fizzBuzz db 'Fizzbuzz!',10
+	fizzBuzzLen equ $ - fizzBuzz
+
+	neither db 'Not fizz, buzz or fizzbuzz!',10
+	neitherLen equ $ - neither
